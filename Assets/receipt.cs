142391d0997108fa3f6b657ab49b131b31c0ef92
Duke Woy <taolimon@disroot.org>
2023-04-01 17:44:03 +0100

    started receipt template

---
+++ Assets/receipt.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Random;
+
+public class receipt : MonoBehaviour
+{
+    public TextMeshPro text;
+    int receipt_id = 0
+    Random random = new Random();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
